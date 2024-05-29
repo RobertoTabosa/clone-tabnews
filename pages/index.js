e0d8f81d275ce88265694e3791e84b76381da1b7
RT <84072431+RobertoTabosa@users.noreply.github.com>
2024-05-29 23:24:42 +0000

    change the sentence

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home () {
     return <h1>We are missing you, My Beautiful Princess.
-        I love you and if you love me, give a laugh. ;)
+        I love you and if you love me, give a big laugh. ;)
     </h1>
 }
 export default Home;
