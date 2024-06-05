defc766d463bd7aa9ef35c822b7b141321b04e4f
RT <84072431+RobertoTabosa@users.noreply.github.com>
2024-06-05 16:23:04 +0000

    actualization

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home () {
     return <h1>We are missing you, My Beautiful Princess.
-        I love you and if you love me, give a big laugh. ;)
+        I missing you a lot ;)
     </h1>
 }
 export default Home;
