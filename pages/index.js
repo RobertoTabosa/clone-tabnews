bdc612018e8bcd491b3b0a342b0c26840bf165f3
RT <84072431+RobertoTabosa@users.noreply.github.com>
2024-06-12 19:25:16 +0000

    Changing sentence again

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
 function Home() {
-  return (
-    <h1>
-      We are missing you Ana Beatriz, My Beautiful Princess. I missing you a lot
-      ;)
-    </h1>
-  );
+  return <h1>Feliz dia dos namorados, Galega! Te amo! </h1>;
 }
 export default Home;
